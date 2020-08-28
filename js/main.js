1acfcfcec34dfea8b4fb13e61e59e98ce046814b
Edward Leung <edward_leung_12@hotmail.com>
2020-08-28 21:06:15 +0100

    Initial commit

---
+++ js/main.js
@@ -0,0 +1,30 @@
+// Listen for form submit
+document.getElementById("myForm").addEventListener("submit", saveItemList);
+
+// Run function to save Item List
+function saveItemList(e) {
+  // Get form values
+  const itemName = document.getElementById("itemName").value;
+  const itemUrl = document.getElementById("itemUrl").value;
+
+  // Save as an JS array object to store in local storage
+  let itemList = {
+    name: itemName,
+    url: itemUrl,
+  };
+
+  // Check if local itemList is in storage
+  if (localStorage.getItem("itemLists") === null) {
+    // Init an array
+    let itemLists = [];
+    // Push new item into the array
+    itemLists.push(itemList);
+    // Set to LocalStorage
+    // itemLists is an array of objects, stringify coverts it to a JSON string
+    localStorage.setItem("itemLists", JSON.stringify(itemLists));
+  } else {
+  }
+
+  // Prevent form from submitting
+  e.preventDefault();
+}
